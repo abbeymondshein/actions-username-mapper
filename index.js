f01d2b9fbba41ef7dc53d7d5cde9c6e1d91e4059
Abbey Mondshein <5883999+abbeymondshein@users.noreply.github.com>
2020-11-23 11:05:23 -0500

    init username mapping action

---
+++ index.js
@@ -0,0 +1,17 @@
+const core = require("@actions/core");
+const fs = require("fs");
+
+(async () => {
+  try {
+    let file = core.getInput("file", { required: true });
+    let githubUsername = core.getInput("github-username", { required: true });
+
+    let data = fs.readFileSync(file, "utf8");
+    let obj = JSON.parse(data);
+    console.log("PARSED DATA:", obj);
+    core.setOutput("slack_id", obj[githubUsername]);
+  } catch (error) {
+    core.setFailed(error.message);
+    throw error;
+  }
+})();
