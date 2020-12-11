14de8e9c9f243fea741ae3d82c258b86340bc320
Abbey Mondshein <5883999+abbeymondshein@users.noreply.github.com>
2020-12-11 13:52:51 -0500

    fallback case uses githubUsername

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const core = require("@actions/core");
-const fs = require("fs");
 
 (async () => {
   try {
     console.log(process.cwd());
     let file = core.getInput("file", { required: true });
     let githubUsername = core.getInput("github-username", { required: true });
-    let obj = JSON.parse(file);
-    core.setOutput("slack_id", obj[0][githubUsername]);
+    let parsedMapping = JSON.parse(file);
+    const slackId = parsedMapping[0][githubUsername]
+      ? parsedMapping[0][githubUsername]
+      : githubUsername;
+    core.setOutput("slack_id", slackId);
   } catch (error) {
     core.setFailed(error.message);
     throw error;
